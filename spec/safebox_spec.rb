7b1bdae822d567539f19dcda1d5ecfae46ac4e2d
Kim Burgestrand <kim@elabs.se>
2015-09-29 13:45:54 +0200

    Nicklasify quotes.

--- spec/safebox_spec.rb
+++ spec/safebox_spec.rb
@@ -1,18 +1,18 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Safebox do
-  it 'has a version number' do
+  it "has a version number" do
     expect(Safebox::VERSION).not_to be nil
   end
 
-  it 'can encrypt and decrypt' do
+  it "can encrypt and decrypt" do
     password = "super secret password"
     message = "Elvis lives!!!"
 
     expect(Safebox.decrypt(password, Safebox.encrypt(password, message))).to eq(message)
   end
 
-  it 'raises an error when decrypting a box with the wrong version' do
+  it "raises an error when decrypting a box with the wrong version" do
     ciphertext = [1337, "smaller salt in this version", "this is the ciphertext"].pack("Q<a*a*")
 
     expect {
